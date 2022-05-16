0231a07267187acce17814e9dbdf84bca342060d
Casey <chesedcaseycollins@gmail.com>
2022-05-16 12:31:23 -0400

    Gallery is in some working order. Subject to design change

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,5 +1,41 @@
+let liA = document.querySelector('#a');
+let liB = document.querySelector('#b');
+let liC = document.querySelector('#c');
 let desc = document.createElement('p');
-desc.setAttribute('id', 'description');
-desc.innerHTML = "hello";
-let main = document.querySelector('main');
-main.appendChild(desc);
+desc.id = "description";
+liA.appendChild(desc);
+let items;
+
+
+liA.addEventListener('click', () => {
+    document.querySelector('#description').remove;
+    desc.innerHTML = liA.firstChild.getAttribute('alt');
+    liA.appendChild(desc);
+    items = document.querySelectorAll('.gallery');
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.remove('display');
+    }
+    liA.classList.add('display');
+});
+
+liB.addEventListener('click', () => {
+    document.querySelector('#description').remove;
+    desc.innerHTML = liB.firstChild.getAttribute('alt');
+    liB.appendChild(desc);
+    items = document.querySelectorAll('.gallery');
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.remove('display');
+    }
+    liB.classList.add('display');
+});
+
+liC.addEventListener('click', () => {
+    document.querySelector('#description').remove;
+    desc.innerHTML = liC.firstChild.getAttribute('alt');
+    liC.appendChild(desc);
+    items = document.querySelectorAll('.gallery');
+    for (let i = 0; i < items.length; i++) {
+        items[i].classList.remove('display');
+    }
+    liC.classList.add('display');
+});
